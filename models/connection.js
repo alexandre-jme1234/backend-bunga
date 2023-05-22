c24fa67f125d01193d5d5d585a180a76c9671df2
bade <infoabde@gmail.com>
2023-05-22 11:29:19 +0200

    route bungalow/dispo

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://infoabde:Mongodb2-@capsule.uvsqdnq.mongodb.net/bungaData';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
--- routes/bungalow.js
+++ routes/bungalow.js
@@ -7,4 +7,69 @@ router.post("/", async (req, res) => {
   res.json(newDoc);
 });
 
+//---------route bungalow dispo
+
+router.get("/dispo", async (req, res) => {
+  // try {
+    const destination = req.query.destination;
+    // formatage input dateSouhait = date("2023-08-01T00:00:00.000Z")
+    const dateSouhait = new Date(req.query.dateSouhait);
+    const inputcapacite = parseInt(req.query.bodyCounter);
+    console.log("datesouhait", dateSouhait)
+    console.log("destination", destination)
+    console.log("inputcapacite", inputcapacite)
+    let matchCritere = {}
+    //-- Filtre de recherche en fonction destination
+    if (destination !== undefined) {
+    matchCritere = {
+      $or: [{ ville: destination }, { departementNom: destination }],
+    };}
+    //-- Filtre de capacite du bungalow
+    if (inputcapacite) {
+      const parsedCapacite = parseInt(inputcapacite);
+      if (!isNaN(parsedCapacite)) {
+        matchCritere.capacite = { $gte: parsedCapacite };
+      }
+    }
+    //-- Filtre date de demarrage demarrage souhaité de la location
+    if (dateSouhait) {
+      const parsedDate = new Date(dateSouhait);
+      if (!isNaN(parsedDate.getTime())) {
+        matchCritere.$and = [
+          { "disponibilites.dateDebut": { $lte: parsedDate } },
+          { "disponibilites.dateFin": { $gte: parsedDate } },
+        ];
+      }
+    }
+    // 
+
+    const bungalows = await Bungalow.aggregate([
+      {
+        $lookup: {
+          from: "disponibilites",
+          localField: "_id",
+          foreignField: "bungalow",
+          as: "disponibilites",
+        },
+      },
+      {
+        //preparation data capaciteAdulte + capaciteEnfant pour filtre capacité
+
+        $addFields: {
+          capacite: { $add: ["$capaciteAdulte", "$capaciteEnfant"] },
+        },
+      },
+      {
+        $match: matchCritere,
+      },
+    ]);
+    res.json(bungalows);
+  // } catch (error) {
+  //   res.status(500).send(error);
+  // }
+});
+
+
+
+
 module.exports = router;
