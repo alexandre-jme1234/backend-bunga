17aa24a58962b6c0c074c10edee1c055131d3f8a
bade <infoabde@gmail.com>
2023-05-25 17:54:24 +0200

    vercel

--- routes/bungalow.js
+++ routes/bungalow.js
@@ -10,68 +10,64 @@ router.post("/", async (req, res) => {
 //---------route bungalow dispo
 
 router.get("/dispo", async (req, res) => {
-  console.log(req.query.dateString)
-    const destination = req.query.destination;
-    // formatage input dateSouhait = date("2023-08-01T00:00:00.000Z")
-    // ====> remettre la date en dynamique
-    const dateSouhait = new Date(req.query.dateString);
-    // const dateSouhait = new Date(2023-07-19);
-    const inputcapacite = parseInt(req.query.bodyCounter);
-    console.log("datesouhait", dateSouhait)
-    console.log("destination", destination)
-    console.log("inputcapacite", inputcapacite)
-    let matchCritere = {}
-    //-- Filtre de recherche en fonction destination
-    if (destination !== undefined) {
+  console.log(req.query.dateString);
+  const destination = req.query.destination;
+
+  // formatage input dateSouhait = date("2023-08-01T00:00:00.000Z")
+
+  const dateSouhait = new Date(req.query.dateString);
+
+  const inputcapacite = parseInt(req.query.bodyCounter);
+  console.log("datesouhait", dateSouhait);
+  console.log("destination", destination);
+  console.log("inputcapacite", inputcapacite);
+  let matchCritere = {};
+  //-- Filtre de recherche en fonction destination
+  if (destination !== undefined) {
     matchCritere = {
       $or: [{ ville: destination }, { departementNom: destination }],
-    };}
-    //-- Filtre de capacite du bungalow
-    if (inputcapacite) {
-      const parsedCapacite = parseInt(inputcapacite);
-      if (!isNaN(parsedCapacite)) {
-        matchCritere.capacite = { $gte: parsedCapacite };
-      }
+    };
+  }
+  //-- Filtre de capacite du bungalow
+  if (inputcapacite) {
+    const parsedCapacite = parseInt(inputcapacite);
+    if (!isNaN(parsedCapacite)) {
+      matchCritere.capacite = { $gte: parsedCapacite };
     }
-    //-- Filtre date de demarrage demarrage souhaité de la location
-    if (dateSouhait) {
-      const parsedDate = new Date(dateSouhait);
-      if (!isNaN(parsedDate.getTime())) {
-        matchCritere.$and = [
-          { "disponibilites.dateDebut": { $lte: parsedDate } },
-          { "disponibilites.dateFin": { $gte: parsedDate } },
-        ];
-      }
+  }
+  //-- Filtre date de demarrage demarrage souhaité de la location
+  if (dateSouhait) {
+    const parsedDate = new Date(dateSouhait);
+    if (!isNaN(parsedDate.getTime())) {
+      matchCritere.$and = [
+        { "disponibilites.dateDebut": { $lte: parsedDate } },
+        { "disponibilites.dateFin": { $gte: parsedDate } },
+      ];
     }
-    // 
+  }
+  //
 
-    const results  = await Bungalow.aggregate([
-      {
-        $lookup: {
-          from: "disponibilites",
-          localField: "_id",
-          foreignField: "bungalow",
-          as: "disponibilites",
-        },
+  const results = await Bungalow.aggregate([
+    {
+      $lookup: {
+        from: "disponibilites",
+        localField: "_id",
+        foreignField: "bungalow",
+        as: "disponibilites",
       },
-      {
-        //preparation data capaciteAdulte + capaciteEnfant pour filtre capacité
+    },
+    {
+      //preparation data capaciteAdulte + capaciteEnfant pour filtre capacité
 
-        $addFields: {
-          capacite: { $add: ["$capaciteAdulte", "$capaciteEnfant"] },
-        },
+      $addFields: {
+        capacite: { $add: ["$capaciteAdulte", "$capaciteEnfant"] },
       },
-      {
-        $match: matchCritere,
-      },
-    ]);
-    res.json({ success: true, results});
-  // } catch (error) {
-  //   res.status(500).send(error);
-  // }
+    },
+    {
+      $match: matchCritere,
+    },
+  ]);
+  res.json({ success: true, results });
 });
-
-
-
 
 module.exports = router;
